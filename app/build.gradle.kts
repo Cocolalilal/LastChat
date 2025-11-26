621e07adced8207fb7c421981e022d972f045735
Cocolalilal <cocolalilal@users.noreply.github.com>
2025-11-27 00:50:04 +0200

    changed the app version to get ready for uploading the latest tweaks to github

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "lastchat.rikkafork.cocolal"
         minSdk = 31
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.0.5"
+        versionCode = 6
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
