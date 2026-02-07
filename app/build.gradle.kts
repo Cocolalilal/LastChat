27c6eaddd5ac024b544f1f5cbd9ecdbb949a02a9
Cocolalilal <109036699+Cocolalilal@users.noreply.github.com>
2026-02-07 19:49:09 +0100

    updated app version to 1.3.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "lastchat.rikkafork.cocolal"
         minSdk = 31
         targetSdk = 36
-        versionCode = 24
-        versionName = "1.3.3"
+        versionCode = 25
+        versionName = "1.3.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
